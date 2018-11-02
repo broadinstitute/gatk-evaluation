7f06736824740c1a35c5a6c68e861563f3a397d1
Samuel Lee <lee.samuel.k@gmail.com>
2018-11-02 16:33:34 -0400

    progress

---
+++ pipeline-optimizer/launch_study.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+
+import argparse
+import advisor_client.client as AdvisorClient
+from cromwell_tools.cromwell_api import CromwellAPI
+from cromwell_tools.cromwell_auth import CromwellAuth
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--advisor_server")
+parser.add_argument("--cromwell_server")
+parser.add_argument("--workflow_wdl")
+parser.add_argument("--template_json")
+parser.add_argument("--hyperparameters_json")
+parser.add_argument("--womtool_path")
+args = parser.parse_args()
+
+
+def main():
+    print "Validating workflow..."
+    CromwellAPI.validate_workflow(args.workflow_wdl, args.womtool_path)
+    
+    client = AdvisorClient.AdvisorClient(endpoint=args.advisor_server)
+    
+    cromwell_auth = CromwellAuth(url=args.cromwell_server, header=None, auth=None)
+    
+    print CromwellAPI.health(cromwell_auth)
+    
+#    CromwellAPI.submit(cromwell_auth, args.workflow_wdl, args.template_json)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pipeline-optimizer/launch_study.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# stop script if there is an error
+set -e
+
+# parser arguments
+while getopts "d:a:c:w:t:h:" option; do
+	case "$option" in
+		d) DOCKER="$OPTARG" ;;
+		a) ADVISOR_SERVER="$OPTARG" ;;
+		c) CROMWELL_SERVER="$OPTARG" ;;
+		w) WORKFLOW_WDL="$OPTARG" ;;
+		t) TEMPLATE_JSON="$OPTARG" ;;
+		h) HYPERPARAMETERS_JSON="$OPTARG" ;;
+	esac
+done
+
+if [ ! $DOCKER ] | [ ! $ADVISOR_SERVER ] | [ ! $CROMWELL_SERVER ] | [ ! $WORKFLOW_WDL ] | [ ! $TEMPLATE_JSON ] | [ ! $HYPERPARAMETERS_JSON ]; then
+	echo "All arguments are required."
+	exit 1
+fi
+
+echo "DOCKER: ${DOCKER}"
+echo "ADVISOR_SERVER: ${ADVISOR_SERVER}"
+echo "CROMWELL_SERVER: ${CROMWELL_SERVER}"
+echo "WORKFLOW_WDL: ${WORKFLOW_WDL}"
+echo "TEMPLATE_JSON: ${TEMPLATE_JSON}"
+echo "HYPERPARAMETERS_JSON: ${HYPERPARAMETERS_JSON}"
+
+docker run \
+    --network="host" \
+    -v $PWD/launch_study.py:/launch_study.py \
+    -v $PWD/${WORKFLOW_WDL}:/workflow.wdl \
+    -v $PWD/${TEMPLATE_JSON}:/template.json \
+    -v $PWD/${HYPERPARAMETERS_JSON}:/hyperparameters.json \
+    -it ${DOCKER} \
+    python2 /launch_study.py \
+        --advisor_server ${ADVISOR_SERVER} \
+        --cromwell_server ${CROMWELL_SERVER} \
+        --workflow_wdl /workflow.wdl \
+        --template_json /template.json \
+        --hyperparameters_json /hyperparameters.json \
+        --womtool_path /usr/local/bin/womtool/womtool-35.jar
