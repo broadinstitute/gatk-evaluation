b627f0efe5a9cd2b06c6d32d942ab8c07722c4b6
samuelklee <samuelklee@users.noreply.github.com>
2018-12-11 16:42:58 -0500

    Added example gCNV hyperparameter optimization. (#20)

---
+++ CNV/gCNV/hyperparameter-optimization-example/launch_study.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# stop script if there is an error
+set -e
+
+ADVISOR_SERVER="http://127.0.0.1:8001"
+CROMWELL_SERVER="http://cromwell-v33.dsde-methods.broadinstitute.org"
+STUDY_NAME="toy-gCNV-opt"
+ALGORITHM="BayesianOptimization"
+
+docker run \
+    --network host \
+    -v $PWD/../../../pipeline-optimizer/launch_study.py:/launch_study.py \
+    -v $PWD/workflow.wdl:/workflow.wdl \
+    -v $PWD/template.json:/template.json \
+    -v $PWD/scan.json:/scan.json \
+    -it us.gcr.io/broad-dsde-methods/gatk-evaluation:pipeline-optimizer \
+    python2 /launch_study.py \
+        --advisor_server ${ADVISOR_SERVER} \
+        --cromwell_server ${CROMWELL_SERVER} \
+        --study_name ${STUDY_NAME} \
+        --algorithm ${ALGORITHM} \
+        --workflow_wdl /workflow.wdl \
+        --template_json /template.json \
+        --scan_json /scan.json \
+        --womtool_path /usr/local/bin/womtool/womtool-36.jar 2>&1 | tee -a launch_study.log
