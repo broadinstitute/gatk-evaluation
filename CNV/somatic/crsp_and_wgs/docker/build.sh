11257b0f55a3e11a77fb3ce6fe1da25406f6a873
tmelman <tmelman@broadinstitute.org>
2022-04-14 10:10:44 -0400

    updated docker

--- CNV/somatic/crsp_and_wgs/docker/build.sh
+++ CNV/somatic/crsp_and_wgs/docker/build.sh
@@ -5,7 +5,7 @@ set -e
 REPO=us.gcr.io/broad-dsde-methods/gatk-evaluation
 PROJECT=cnv_somatic_1p5
 EVALUATION_NAME=crsp_and_wgs
-VERSION=1.1
+VERSION=1.2   #1.1
 FINAL_GCR_TAG=${REPO}/${PROJECT}:${EVALUATION_NAME}_${VERSION}
 echo "Building ${FINAL_GCR_TAG}..."
 gcloud docker -- build -t ${FINAL_GCR_TAG} -f Dockerfile .
